43c64eefc8e9b37c80703611da8628577922bf1c
Junsu Lee <ljs93kr@gmail.com>
2018-11-07 02:53:42 +0000

    convert for yumi colorization

--- tools/create_imagenet_list.py
+++ tools/create_imagenet_list.py
@@ -1,10 +1,9 @@
 import os
 
 f = open('data/train.txt', 'w')
-imagenet_basepath = './data/imagenet/'
-for p1 in os.listdir(imagenet_basepath):
-  if p1.startswith('n'):
-    for p2 in os.listdir(imagenet_basepath + p1 + '/'):
-      image = os.path.abspath(imagenet_basepath + p1 + '/' + p2)
-      f.write(image + '\n')
+imagenet_basepath = './data/train'
+for each_image_name in os.listdir(imagenet_basepath):
+    image_path = os.path.abspath(os.path.join(imagenet_basepath, each_image_name))
+    print(image_path)
+    f.write(image_path + '\n')
 f.close()
